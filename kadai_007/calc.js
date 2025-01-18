97973792c647b09beca25c6c65291cec2f87ed14
yuichos <dance.dreamy@gmail.com>
2025-01-19 00:16:09 +0900

    kadai007を上書きアップロード

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,19 +1,11 @@
 let num = 15
 
-if(num % 3 == 0 ){
+if(num % 3 == 0 && num % 5 == 0){
+  console.log("3と5の倍数です")
+}else if(num % 3 == 0 ){
   console.log('3の倍数です')
-}
-
-if(num % 5 == 0){
+}else if(num % 5 == 0){
   console.log('5の倍数です')
-}
-
-if(num % 3 == 0){
-  if(num % 5 == 0){
-    console.log('3と5の倍数です')
-  }
-}
-
-else{
+}else{
   console.log(num)
 }
